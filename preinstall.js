4f29df1308939f3841b3931f250c1385bb603723
Jordan Mackie <jmackie@protonmail.com>
2018-11-22 12:26:55 +0000

    Fix preinstall script for windows

--- preinstall.js
+++ preinstall.js
@@ -8,7 +8,10 @@ const version = require('./package.json').version;
 const platform =
     { win32: 'win64', darwin: 'macos' }[process.platform] || 'linux64';
 
+const windows = platform === 'win64';
+
 const binDir = 'bin';
+const exeName = windows ? 'elm-smuggle.exe' : 'elm-smuggle';
 const release = `https://github.com/jmackie/elm-smuggle/releases/download/v${version}/${platform}.tar.gz`;
 
 function throwError(error) {
@@ -28,16 +31,13 @@ https
         //
         response.pipe(
             tar
-                .x({ cwd: binDir, strip: 1 }, ['elm-smuggle/elm-smuggle'])
+                .x({ cwd: binDir, strip: 1 }, [`elm-smuggle/${exeName}`])
                 .on('finish', () => {
                     // Because we need to point to one file in the `bin` section
                     // of `package.json` we always give the executable an `.exe`
-                    // extension, because unix doesn't care.
-                    if (platform !== 'win64') {
-                        mv(
-                            `${binDir}/elm-smuggle`,
-                            `${binDir}/elm-smuggle.exe`
-                        );
+                    // extension because unix doesn't actually care.
+                    if (!windows) {
+                        mv(`${binDir}/${exeName}`, `${binDir}/${exeName}.exe`);
                     }
                 })
         );
